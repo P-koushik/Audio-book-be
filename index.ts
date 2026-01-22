cce736d55c1e7b2248521a9cfb72fa60f30e6acf
P-koushik <pkoushik224@gmail.com>
2026-01-23 01:02:36 +0530

    feat: added linting and format

--- index.ts
+++ index.ts
@@ -1,21 +1,24 @@
-import mongoose from "mongoose"
-import express from "express"
-import { env } from "./constants/env"
+import mongoose from "mongoose";
+import express from "express";
+import { env } from "./constants/env";
 
-const app = express()
+const app = express();
 
 if (!env.MONGO_URL) {
-    console.log("MONGO_URL environment variable is not set")
+  console.log("MONGO_URL environment variable is not set");
 }
 
-mongoose.connect(env.MONGO_URL).then(() => {
-    console.log("Mongo Db is connected")
-}).catch((error) => {
-    console.log(error)
-})
+mongoose
+  .connect(env.MONGO_URL)
+  .then(() => {
+    console.log("Mongo Db is connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-app.use(express.json())
+app.use(express.json());
 
 app.listen(env.PORT, () => {
-    console.log(`Server is running in port ${env.PORT}`)
-})
+  console.log(`Server is running in port ${env.PORT}`);
+});
--- models/user.ts
+++ models/user.ts
@@ -1,12 +1,14 @@
-import mongoose, { Schema, model } from "mongoose"
-import { TUser } from "../types/user"
+import mongoose, { Schema, model } from "mongoose";
+import { TUser } from "../types/user";
 
 const user_schema = new Schema<TUser>(
-    {
-        email: { type: String, required: true, unique: true },
-        name: { type: String, required: true }
-    }, {
-    timestamps: true
-})
+  {
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-export const User = mongoose.models.User || model<TUser>('User', user_schema)
+export const User = mongoose.models.User || model<TUser>("User", user_schema);
